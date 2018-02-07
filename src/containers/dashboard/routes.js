38993af1dfbf9ab299b87354ff56558fdc4e4713
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-02-07 11:29:58 -0200

    route: added new routes for equipamento components.
    Was added new routes for equipamento components.

--- src/containers/dashboard/routes.js
+++ src/containers/dashboard/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Main } from './scenes/main/main';
-import { Equipamentos } from './scenes/equipamentos/equipamentos'
+import { Novo } from './scenes/equipamentos/containers/novo';
+import { RoutesEquipamentos } from './scenes/equipamentos/equipamentos';
 
 export const DashBoardRoutes = () => (
   <Switch>
     <Route exact path='/dashboard/main' component={Main}/>
-    <Route path='/dashboard/equipamentos' component={Equipamentos}/>
-    <Route path='/dashboard' component={Main}/>
+    <Route path='/dashboard/equipamentos' component={RoutesEquipamentos}/>
   </Switch>
 );
--- src/containers/dashboard/scenes/equipamentos/equipamentos.js
+++ src/containers/dashboard/scenes/equipamentos/equipamentos.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Novo, Gerenciar, Detalhes } from './containers';
 
-export const Equipamentos = () => (
-  <div>
-    <h1>Equipamentos</h1>
-  </div>
-);
+export const RoutesEquipamentos = () => (
+  <Switch>
+    <Route path='/dashboard/equipamentos/novo' component={Novo}/>
+    <Route path='/dashboard/equipamentos/gerenciar' component={Gerenciar}/>
+    <Route path='/dashboard/equipamentos/detalhes' component={Detalhes}/>
+  </Switch>
+)
